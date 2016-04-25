721ad74d2c4b9bd3d2601e64798cd31a210b2ece
unknown <you@example.com>
2016-04-25 11:03:59 +0200

    Creada la clase enum Option

---
+++ Option.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Option - write a description of the enum class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Option
+{
+    AL, SALIR, AYUDA, LOOK, EAT, BACK, TAKE, DROP, ITEMS, UNKNOWN
+}
